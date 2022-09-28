5e02178cb0ee21038a0b3f106fe00aa5641e0a82
kevinrss01 <kevin.rousseau3@gmail.com>
2022-09-28 12:05:13 +0200

    first commit

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -4,6 +4,11 @@ import * as Yup from 'yup'; //For the form validation
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../helpers/AuthContext';
+import { BiUserCircle } from 'react-icons/bi';
+import { RiKey2Fill } from 'react-icons/ri';
+import { AiFillFacebook } from 'react-icons/ai';
+import { AiOutlineTwitter } from 'react-icons/ai';
+import { AiOutlineGoogle } from 'react-icons/ai';
 
 function Registration() {
 	const initialValues = {
@@ -14,7 +19,7 @@ function Registration() {
 	const [showErrorMessage, setShowErrorMessage] = useState(false);
 	const { setAuthState } = useContext(AuthContext);
 
-	//CONTROLE INPUT
+	//CONTROLE INPUTs
 	const validationSchema = Yup.object().shape({
 		username: Yup.string()
 			.min(3, "Nom d'utilisateur trop court")
@@ -52,41 +57,103 @@ function Registration() {
 
 	return (
 		<div className="registrationContainer">
-			<h1>Créer un compte</h1>
-			<Formik
-				initialValues={initialValues}
-				onSubmit={onSubmit}
-				validationSchema={validationSchema}
-			>
-				<Form className="formCreateUserContainer">
-					<label>Nom d'utilisateur :</label>
-					{showErrorMessage && (
-						<label className="errorMessage">
-							Nom d'utilisateur déja pris, veuillez en choisir un autre.
-						</label>
-					)}
+			<div className="containerLeftSide">
+				<Formik
+					initialValues={initialValues}
+					onSubmit={onSubmit}
+					validationSchema={validationSchema}
+				>
+					<Form className="formCreateUserContainer">
+						<h1>Créer un compte</h1>
+						{showErrorMessage && (
+							<label className="errorMessage">
+								Nom d'utilisateur déja pris, veuillez en choisir un autre.
+							</label>
+						)}
 
-					<ErrorMessage name="username" component="span" />
-					<Field
-						autoComplete="off"
-						id="inputUsername"
-						name="username"
-						placeholder="Nom d'utilisateur"
-					/>
-					<label>Mot de passe :</label>
-					<ErrorMessage name="password" component="span" />
-					<Field
-						type="password"
-						id="inputPassword"
-						name="password"
-						placeholder="Mot de passe"
-					/>
+						<ErrorMessage name="username" component="span" />
+						<div className="containerUserNameAndPassword flex_center">
+							<BiUserCircle className="iconInput flex_center" />
+							<Field
+								autoComplete="off"
+								id="inputUsername"
+								name="username"
+								placeholder="Nom d'utilisateur"
+							/>
+						</div>
 
-					<button type="submit" className="registrationBtn">
-						Créer mon compte
-					</button>
-				</Form>
-			</Formik>
+						<ErrorMessage name="password" component="span" />
+						<div className="containerUserNameAndPassword flex_center">
+							<RiKey2Fill className="iconInput flex_center" />
+							<Field
+								type="password"
+								id="inputPassword"
+								name="password"
+								placeholder="Mot de passe"
+							/>
+						</div>
+
+						<div className="checkBoxAndLogin flex_center">
+							<div className="souvenir flex_center">
+								<input type="checkbox" className="rememberCheckbox" />
+								<span>Se souvenir de moi</span>
+							</div>
+
+							<button type="submit" className="registrationBtn">
+								Créer mon compte
+							</button>
+						</div>
+
+						<div className="regisForgotPassContainer">
+							<span
+								className="register"
+								onClick={() => {
+									navigate('/login');
+								}}
+							>
+								Se connecter
+							</span>
+							<span></span>
+						</div>
+
+						<div className="divider">
+							<hr class="solid"></hr>
+							<span>ou</span>
+							<hr class="solid"></hr>
+						</div>
+
+						<div className="containerLoginWith">
+							<div className="loginWith">
+								<div className="containerLogo colorFb1">
+									<AiFillFacebook className="iconSocialMedia" />
+								</div>
+								<div className="containerText colorFb2">
+									Connexion avec Facebook
+								</div>
+							</div>
+
+							<div className="loginWith">
+								<div className="containerLogo colorTwitter1">
+									<AiOutlineTwitter className="iconSocialMedia" />
+								</div>
+								<div className="containerText colorTwitter2">
+									Connexion avec Twitter
+								</div>
+							</div>
+
+							<div className="loginWith">
+								<div className="containerLogo colorGoogle1">
+									<AiOutlineGoogle className="iconSocialMedia" />
+								</div>
+								<div className="containerText colorGoogle2">
+									Connexion avec Google
+								</div>
+							</div>
+						</div>
+					</Form>
+				</Formik>
+			</div>
+			<div className="containerRightSide"></div>
 		</div>
 	);
 }
